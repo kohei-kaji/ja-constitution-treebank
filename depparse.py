1a2db4e0c3cbb640768779b4789f67d8c0959876
kohei-kaji <kohei.kajikawa1223@gmail.com>
2026-06-08 22:02:10 +0900

    Add silver parses of constitution

--- depparse.py
+++ depparse.py
@@ -1,14 +1,15 @@
 import argparse
 import spacy
-from pathlib import Path
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input", help="Input text file")
 parser.add_argument("output", help="Output CoNLL-U file")
 
-def to_conllu(doc) -> str:
+def to_conllu(doc, start_sent_id=1) -> tuple[str, int]:
     lines = []
-    for sent_i, sent in enumerate(doc.sents, 1):
+    sent_i = start_sent_id
+
+    for sent in doc.sents:
         lines.append(f"# sent_id = {sent_i}")
         lines.append(f"# text = {sent.text}")
         for token in sent:
@@ -30,24 +31,31 @@ def to_conllu(doc) -> str:
                 ])
             )
         lines.append("")
-    return "\n".join(lines)
+        sent_i += 1
 
+    return "\n".join(lines), sent_i
 
 def main():
     args = parser.parse_args()
 
-    with open(args.input, encoding="utf-8") as f:
-        text = f.read()
-
     nlp = spacy.load("ja_ginza", config={"components": {"compound_splitter": {"split_mode": "C"}}})
-    doc = nlp(text)
 
-    output = to_conllu(doc)
-    print(output)
+    with open(args.input, "r", encoding="utf-8") as fin, open(args.output, "w", encoding="utf-8") as fout:
 
-    out_path = Path(args.output)
-    out_path.write_text(output, encoding="utf-8")
+        current_sent_id = 1
 
+        for line in fin:
+            line = line.strip()
+            if not line:
+                continue
+
+            doc = nlp(line)
+
+            output_str, current_sent_id = to_conllu(doc, start_sent_id=current_sent_id)
+
+            if output_str:
+                fout.write(output_str + "\n")
+                print(output_str)
 
 if __name__ == "__main__":
     main()
--- parse.sh
+++ parse.sh
@@ -1,1 +1,2 @@
 uv run depparse.py preamble.txt silver/preamble-ginza.conllu
+uv run depparse.py constitution.txt silver/constitution-ginza.conllu
